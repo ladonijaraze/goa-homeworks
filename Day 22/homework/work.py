c6c93448ac81987044fa57fb8f73ddf625bd92d3
ladonijaraze <ladonijaraze@gmail.com>
2024-11-19 17:50:33 +0400

    Add files via upload

---
+++ Day 22/homework/work.py
@@ -0,0 +1,28 @@
+#1
+names = ["დავით", "გიორგი", "თამარი", "დავით", "მარიამ", "დავით"]
+name_to_count = "დავით"
+count = names.count(name_to_count)
+print(count)
+
+#2
+numbers = [1, 2, 3, 4, 5]
+numbers.reverse()
+print(numbers)
+
+#3
+list_numbers = [1, 2, 3]
+new_list = list_numbers * len(list_numbers)  
+print(new_list)
+
+#4
+arr = ["Python", "html", "C++","js"]
+arr.insert(1, "დავითი")  
+print(arr)
+
+#5
+list = [1, 2, 3, 4, 2, 5, 2]
+remove = 2
+
+count = list.count(remove)  
+list.remove(remove)  
+print(list)
