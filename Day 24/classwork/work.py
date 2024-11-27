39e09f822e7df65ac04ec7a2472374ed112b4fcb
ladonijaraze <ladonijaraze@gmail.com>
2024-11-27 22:25:36 +0400

    Delete Day 24 directory

--- Day 24/classwork/work.py
+++
@@ -1,5 +0,0 @@
-num=1
-
-
-kk="ramdeni wlis xar{0} rogori amindia ra ricxvia? {1}".format(num,num+4)
-print(kk)
--- Day 24/classwork/work_1.py
+++
@@ -1,7 +0,0 @@
-string = "შაურმა არ არის  ხინკალზე კაი"
-
-split_words = string.split()
-
-joined_string = " 💤 ".join(split_words)
-
-print(joined_string)
